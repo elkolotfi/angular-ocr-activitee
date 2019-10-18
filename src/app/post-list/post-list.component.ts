769fe6b182db02626445dc32b2e3a1c01d1ce35d
elkolotfi <elkolotfi@gmail.com>
2019-10-18 23:37:23 +0200

    post subscription

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,19 +1,29 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Post} from '../../model/Post';
 import {PostService} from '../services/post.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[];
+  postSubscription: Subscription;
 
   constructor(private postService: PostService) { }
 
   ngOnInit() {
-    this.posts = this.postService.posts;
+    this.postSubscription = this.postService.postSubject.subscribe(
+      (posts: Post[]) => {
+          this.posts = posts;
+      });
+    this.postService.emitPost();
+  }
+
+  ngOnDestroy(): void {
+    this.postSubscription.unsubscribe();
   }
 
 }
