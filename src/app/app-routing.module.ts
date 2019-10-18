463473246aef0c765554919725b342624f68075d
elkolotfi <elkolotfi@gmail.com>
2019-10-18 16:17:23 +0200

    init routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Route, RouterModule} from '@angular/router';
+import {PostListComponent} from './post-list/post-list.component';
+import {NewPostComponent} from './new-post/new-post.component';
+
+const routes: Route[] = [
+  { path: 'posts/new', component: NewPostComponent },
+  { path: 'posts', component: PostListComponent },
+  { path: '', redirectTo: 'posts', pathMatch: 'full' },
+  { path: '**', redirectTo: 'posts', pathMatch: 'full' }
+  ];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostListItemComponent } from './post-list-item/post-list-item.component';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { PostListItemComponent } from './post-list-item/post-list-item.component
   ],
   imports: [
     BrowserModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
