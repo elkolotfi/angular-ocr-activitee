25798bc474f76727f596970fd5f4720161c7a832
elkolotfi <elkolotfi@gmail.com>
2019-10-24 09:30:26 +0200

    install firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {Post} from '../model/Post';
+import {Component} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,17 @@ import {Post} from '../model/Post';
 })
 export class AppComponent {
 
+  constructor() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyCYZ6Qf2xeBPMWN9CC7FvA4qj55SqWLqsc',
+      authDomain: 'ocr-angular-activity.firebaseapp.com',
+      databaseURL: 'https://ocr-angular-activity.firebaseio.com',
+      projectId: 'ocr-angular-activity',
+      storageBucket: 'ocr-angular-activity.appspot.com',
+      messagingSenderId: '400003732399',
+      appId: '1:400003732399:web:0582d45cb43e33a70a155a'
+    };
+
+    firebase.initializeApp(firebaseConfig)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { PostListComponent } from './post-list/post-list.component';
-import { PostListItemComponent } from './post-list-item/post-list-item.component';
-import { AppRoutingModule } from './app-routing.module';
-import { NewPostComponent } from './new-post/new-post.component';
-import { HeaderComponent } from './header/header.component';
+import {AppComponent} from './app.component';
+import {PostListComponent} from './post-list/post-list.component';
+import {PostListItemComponent} from './post-list-item/post-list-item.component';
+import {AppRoutingModule} from './app-routing.module';
+import {NewPostComponent} from './new-post/new-post.component';
+import {HeaderComponent} from './header/header.component';
 import {RouterModule} from '@angular/router';
 import {ReactiveFormsModule} from '@angular/forms';
 
