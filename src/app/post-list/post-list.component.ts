11864cd59e1cd118bb0d2eb15a651ee4ba6133f6
elkolotfi <elkolotfi@gmail.com>
2019-10-24 09:31:13 +0200

    [refactor] Input dependency no more needed on post list component

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Post} from '../../model/Post';
 import {PostService} from '../services/post.service';
 import {Subscription} from 'rxjs';
